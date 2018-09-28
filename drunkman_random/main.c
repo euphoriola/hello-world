53d2756fbe9dba0eeb467c3e40964f336739b9ff
euphoriola <euphoriola@google.com>
2018-09-28 16:55:32 +0800

    add random check bar

--- drunkman_random/main.c
+++ drunkman_random/main.c
@@ -33,10 +33,42 @@ int invoke_random(int min, int max, int edge)
     return random;
 }
 
+int man_in_bar(int r, int bar)
+{
+    int min;
+    int max;
+    switch (bar) {
+        case 1:
+            min = 71;
+            max = 100;
+        break;
+        case 2:
+            min = 41;
+            max = 70;
+        break;
+        case 3:
+            min = 11;
+            max = 40;
+        break;
+        default:
+            printf("wtf?!\n");
+        break;
+    }
+
+    if (min <= r && r <= max) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
 
 int main()
 {
     int rlt;
+    int bar;
+    int bar1;
+    int bar2;
     int a = 0;
     int b = 0;
     int c = 0;
@@ -45,6 +77,7 @@ int main()
     double home = 0;
     int i;
     for (i = 0; i < 100000; i++) {
+        bar = invoke_random(1, 3, 0);
         rlt = invoke_random(1, 100, 0);
         // printf("%d\n", invoke_random(1, 100, 0));
         if (0 <= rlt && rlt <= 10) { /*the drunk man in home*/
@@ -63,14 +96,27 @@ int main()
             /* code */
         }
 
+        switch (bar) {
+            case 1: /*police will check bar B and C*/
+                bar1 = 2;
+                bar2 = 3;
+            break;
+            case 2: /*police will check bar A and C*/
+                bar1 = 1;
+                bar2 = 3;
+            break;
+            case 3: /*police will check bar A and B*/
+                bar1 = 1;
+                bar2 = 2;
+            break;
+        }
+
         //start
-        if (rlt <= 70) { /*not in bar A*/
-            if (rlt <= 40) { /*not in bar B*/
-                if (10 < rlt) { /*the man is drunk*/
-                    drunk += 1.0f;
-                } else {
-                    home += 1.0f;
-                }
+        if ((0 == man_in_bar(rlt, bar1)) && (0 == man_in_bar(rlt, bar2))) { /*man not in bar1 or bar2*/
+            if (10 < rlt) { /*the man is drunk*/
+                drunk += 1.0f;
+            } else {
+                home += 1.0f;
             }
         }
 
